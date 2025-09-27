6e1a039e81f57d6472f5856fb387c6a87341e889
AbidHasanPiash <meghpiash@gmail.com>
2025-09-27 13:55:51 +0600

    update

--- app/social/components/Addons.jsx
+++ app/social/components/Addons.jsx
@@ -1,5 +1,8 @@
+'use client'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from 'framer-motion';
+import transitionsIndexed from '@/animations/transitionsIndexed';
 
 export default function Addons() {
     const addons = [
@@ -40,22 +43,29 @@ export default function Addons() {
             </div>
             <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4'>
                 {addons.map((a, index) => (
-                <div key={index} className='flex flex-col items-center p-2 rounded-lg bg-sky-50 ring ring-sky-50 hover:ring-sky-200 shadow-xl hover:shadow-2xl hover:-translate-y-2 transition-transform duration-200'>
-                    <Image
-                        src={a?.icon}
-                        height={50}
-                        width={50}
-                        alt="Instagram- Commerce-icon"
-                        className="bg-blue-500/30 p-2 m-2 rounded-full aspect-square"
-                    />
-                    <h2 className='text-xl font-bold'>{a?.title}</h2>
-                    <p>{a?.description}</p>
-                    <p className='space-x-2'>
-                        <span className='text-xl text-blue-500'>${a?.price}</span>
-                        <span className='text-sm'>/{a?.priceDuration}</span>
-                    </p>
-                </div>
-            ))}
+                    <motion.div 
+                        key={index} 
+                        variants={transitionsIndexed.FadeInFromBottomIndexed}
+                        initial="initial"
+                        whileInView="animate"
+                        viewport={{ once: false, amount: 0.5 }}
+                        custom={index + 1}
+                        className='flex flex-col items-center p-2 rounded-lg bg-sky-50 ring ring-sky-50 hover:ring-sky-200 shadow-xl hover:shadow-2xl'>
+                        <Image
+                            src={a?.icon}
+                            height={50}
+                            width={50}
+                            alt="Instagram- Commerce-icon"
+                            className="bg-blue-500/30 p-2 m-2 rounded-full aspect-square"
+                        />
+                        <h2 className='text-xl font-bold'>{a?.title}</h2>
+                        <p>{a?.description}</p>
+                        <p className='space-x-2'>
+                            <span className='text-xl text-blue-500'>${a?.price}</span>
+                            <span className='text-sm'>/{a?.priceDuration}</span>
+                        </p>
+                    </motion.div>
+                ))}
             </div>
         </section>
     )
