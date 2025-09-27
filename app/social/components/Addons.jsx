12e4aacdbdf37279a2d253e83c4226100336fe45
AbidHasanPiash <meghpiash@gmail.com>
2025-09-27 18:18:34 +0600

    update

--- app/social/components/Addons.jsx
+++ app/social/components/Addons.jsx
@@ -29,36 +29,38 @@ export default function Addons() {
         },
     ]
     return (
-        <section className="max-w-7xl mx-auto p-6 sm:p-8 md:p-10 space-y-10 mb-10">
-            <div className='text-center'>
-                <h1 className='text-2xl sm:text-3xl md:text-5xl font-bold'>Additional Services</h1>
-                <p className='text-lg sm:text-xl'>Customize your plan with these add-ons</p>
-            </div>
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-10'>
-                {addons.map((a, index) => (
-                    <motion.div 
-                        key={index} 
-                        variants={transitionsIndexed.FadeInFromBottomIndexed}
-                        initial="initial"
-                        whileInView="animate"
-                        viewport={{ once: true, amount: 0.5 }}
-                        custom={index + 1}
-                        className='flex flex-col items-center p-2 rounded-lg bg-sky-50 ring ring-sky-50 hover:ring-sky-200 shadow-xl hover:shadow-2xl'>
-                        <Image
-                            src={a?.icon}
-                            height={50}
-                            width={50}
-                            alt="Instagram- Commerce-icon"
-                            className="bg-blue-500/30 p-2 m-2 rounded-full aspect-square"
-                        />
-                        <h2 className='text-xl font-bold'>{a?.title}</h2>
-                        <p>{a?.description}</p>
-                        <p className='space-x-2'>
-                            <span className='text-xl text-blue-500'>${a?.price}</span>
-                            <span className='text-sm'>/{a?.priceDuration}</span>
-                        </p>
-                    </motion.div>
-                ))}
+        <section className="bg-[url('/social/svg/dot.svg')] bg-opacity-25">
+            <div className="max-w-7xl mx-auto p-6 sm:p-8 md:p-10 space-y-10 mb-10 ">
+                <div className='text-center'>
+                    <h1 className='text-2xl sm:text-3xl md:text-5xl font-bold'>Additional Services</h1>
+                    <p className='text-lg sm:text-xl'>Customize your plan with these add-ons</p>
+                </div>
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-10'>
+                    {addons.map((a, index) => (
+                        <motion.div
+                            key={index}
+                            variants={transitionsIndexed.FadeInFromBottomIndexed}
+                            initial="initial"
+                            whileInView="animate"
+                            viewport={{ once: true, amount: 0.5 }}
+                            custom={index + 1}
+                            className='flex flex-col items-center p-2 rounded-lg bg-sky-50 ring ring-sky-50 hover:ring-sky-200 shadow-xl hover:shadow-2xl'>
+                            <Image
+                                src={a?.icon}
+                                height={50}
+                                width={50}
+                                alt="Instagram- Commerce-icon"
+                                className="bg-blue-500/30 p-2 m-2 rounded-full aspect-square"
+                            />
+                            <h2 className='text-xl font-bold'>{a?.title}</h2>
+                            <p>{a?.description}</p>
+                            <p className='space-x-2'>
+                                <span className='text-xl text-blue-500'>${a?.price}</span>
+                                <span className='text-sm'>/{a?.priceDuration}</span>
+                            </p>
+                        </motion.div>
+                    ))}
+                </div>
             </div>
         </section>
     )
