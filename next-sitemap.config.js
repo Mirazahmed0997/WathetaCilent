b4a6afa268246d30bf93cf3ff82cfad9efed4b00
AbidHasanPiash <meghpiash@gmail.com>
2025-09-30 13:06:57 +0600

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next-sitemap').IConfig} */
-
 module.exports = {
     siteUrl: 'https://watheta.com/',
     changefreq: 'daily',
@@ -7,31 +6,22 @@ module.exports = {
     sitemapSize: 5000,
     generateRobotsTxt: true,
     exclude: [
-        '/images/svg.png',       // static image
-        '/admin/*',              // hide admin
-        '/dashboard/*',          // hide dashboard
-        '/api/*',                // hide API routes
+        '/images/svg.png',
+        '/admin/*',
+        '/dashboard/*',
+        '/api/*',
     ],
     alternateRefs: [
-        {
-            href: 'https://es.watheta.com',
-            hreflang: 'es',
-        },
-        {
-            href: 'https://fr.watheta.com',
-            hreflang: 'fr',
-        },
+        { href: 'https://es.watheta.com', hreflang: 'es' },
+        { href: 'https://fr.watheta.com', hreflang: 'fr' },
     ],
-    // Default transformation function
-    transform: async (config, path) => {
-        return {
-            loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
-            changefreq: config.changefreq,
-            priority: config.priority,
-            lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-            alternateRefs: config.alternateRefs ?? [],
-        }
-    },
+    transform: async (config, path) => ({
+        loc: path,
+        changefreq: config.changefreq,
+        priority: config.priority,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        alternateRefs: config.alternateRefs ?? [],
+    }),
     additionalPaths: async (config) => [
         await config.transform(config, '/additional-page'),
     ],
@@ -45,23 +35,12 @@ module.exports = {
                     '/dashboard',
                     '/api',
                     '/images/svg.png',
-                    'https://platform.openai.com/',
-                    'https://webhook.site/',
-                    'https://docs.google.com/document/d/1WqvyMpAJ04r6LztFCC7wczeCuijdO9rIj90hLxhFFG4/edit?usp=sharing',
-                    'https://static.xx.fbcdn.net/rsrc.php/yv/r/B8BxsscfVBr.ico',
-                    'https://dash.watheta.com/',
-                    '/images/svg.png',
-                    'https://websiteapi.watheta.com/',
                 ],
             },
             {
                 userAgent: 'black-listed-bot',
-                disallow: ['/'], // fully block them
+                disallow: ['/'], // block everything for this bot
             },
         ],
-        additionalSitemaps: [
-            //   'https://watheta.com/my-custom-sitemap-1.xml',
-            //   'https://watheta.com/my-custom-sitemap-2.xml',
-        ],
-    },
+    }
 }
